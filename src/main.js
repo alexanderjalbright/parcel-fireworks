58261485095194d0ea4605acfe81a2e02ba7beb4
Alex Albright <alexanderjalbright@gmail.com>
2019-03-18 15:34:00 -0400

    Fireworks!

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-export default () => { alert('it works!');};
+import './main.css';
+import Fireworks from 'fireworks-canvas'; 
+export default () => {  
+    const container = document.getElementById('container')
+    const options = {
+    maxRockets: 30,            // max # of rockets to spawn
+    rocketSpawnInterval: 50, // millisends to check if new rockets should spawn
+    numParticles: 200,        // number of particles to spawn when rocket explodes (+0-10)
+    explosionMinHeight: 0.2,  // percentage. min height at which rockets can explode
+    explosionMaxHeight: 0.9,  // percentage. max height before a particle is exploded
+    explosionChance: 0.08     // chance in each tick the rocket will explode
+    } 
+    const fireworks = new Fireworks(container, options);  
+    fireworks.start();
+};
